854089d10d45fb5ac72229ab7e7875d5c0958d2c
Hp <nadiakarnesya@gmail.com>
2025-01-04 22:17:31 +0700

    initial commit

---
+++ app/src/main/java/com/nadiakr/save_money/MainActivity.java
@@ -0,0 +1,13 @@
+package com.nadiakr.save_money;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
